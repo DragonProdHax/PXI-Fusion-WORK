12c73f58602cfa4691cfbeb8c08fee7d69b89da1
Dragon_X <barron.samuel111@gmail.com>
2024-07-21 20:50:02 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,18 @@
-(async function() {
-    const url = 'https://raw.githubusercontent.com/PXIFusionX/Fusion-Loader/main/Main/Menu';
-    try {
-        const response = await fetch(url);
-        const code = await response.text();
-        eval(code);
-    } catch (error) {
-        console.error('Error fetching or executing the script:', error);
+(function() {
+    // Function to load the script
+    function loadScript(url, callback) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.onload = callback;
+        document.head.appendChild(script);
     }
+
+    // URL of the script from GitHub
+    var scriptUrl = 'https://raw.githubusercontent.com/PXIFusionX/Fusion-Loader/main/Main/Menu';
+
+    // Load the script
+    loadScript(scriptUrl, function() {
+        console.log('Script loaded successfully.');
+    });
 })();
